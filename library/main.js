c0a686e6049087c6f3d2b2b3d6333ead08e9a3bf
LittleMeo <cotikgg@gmail.com>
2024-02-05 09:34:07 +0200

    feat: release menu behavior

--- library/main.js
+++ library/main.js
@@ -1,16 +1,29 @@
 window.onload = function() { // можно также использовать window.addEventListener('load', (event) => {
     
-    const burger = document.querySelector('[data-burger]');
+    const burger = document.querySelector('.button-burger');
+    const burgerOverlay = document.querySelector('.burger__overlay')
+    const navLink = Array.from(document.querySelectorAll('.nav__link'))
     const header = document.querySelector('.header');
     const slider = document.querySelector('.slider');
     const swipe__left = document.querySelector('.swipe__left')
     const swipe__right = document.querySelector('.swipe__right')
     const slider__buttons = document.querySelectorAll('.slider__button-area')
-    
-    
+    const navLinkCallback = (element) => {
+        console.log('Meow', element)
+        element.addEventListener('click', () => {
+            header.classList.remove('nav--visible');
+        });
+    };
+
+    navLink?.map(navLinkCallback);
     burger?.addEventListener('click', () => {
         header.classList.toggle('nav--visible');
     });
+    
+    burgerOverlay?.addEventListener('click', () => {
+        header.classList.toggle('nav--visible');
+    });
+    
     slider.dataset.activeSlideNumber = 1;
     swipe__left?.addEventListener('click',() => {
         dataDown(slider)}); 
@@ -28,35 +41,15 @@ window.onload = function() { // можно также использовать w
             
         }
     }
+
     function dataDown(slider) {
         if (slider.dataset.activeSlideNumber > 1) {
             slider.dataset.activeSlideNumber = --slider.dataset.activeSlideNumber;
             
         }
     }
+
     function dataSet(slider,number) {
         slider.dataset.activeSlideNumber = number; 
-    }
-    document.getElementsById('drop-menu').onmouseover = function(event) {
-        let target = event.target;
-        if (target.className == 'drop-menu') {
-            let s  = target.getElementsByClassName('sub-menu');
-            closeMenu();
-            s[0].style.display = 'block';
-        }
-    }
-    document.onmouseover=function (event) {
-        let target = event.target;
-        console.log(event.target);
-        if (target.className!='drop-menu' && target.className!='sub-menu'){
-            closeMenu();            
-        };
-    }
-    function closeMenu() {
-        let menu = document.getElementById('drop-menu');
-        let subm = document.getElementsByClassName('sub-menu');
-        for (let i=0; i <subm.length; i++) {
-            subm[i].style.display= "none";
-        }   
     }
 }
